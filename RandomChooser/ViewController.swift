2a224e2a7c8f6ddf2c148974f4ab5583a692e5f0
Yuki Waka <yuheey3@yahoo.co.jp>
2021-09-05 13:19:07 -0400

    Initial Commit

---
+++ RandomChooser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomChooser
+//
+//  Created by Yuki Waka on 2021-09-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
