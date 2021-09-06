70dbf705b27d2eb50ca27bd1fb3965e49b86af8e
Yuki Waka <yuheey3@yahoo.co.jp>
2021-09-06 15:03:48 -0400

    result page

--- RandomChooser/ResultViewController.swift
+++ RandomChooser/ResultViewController.swift
@@ -8,9 +8,23 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    @IBOutlet var animal1Lbl : UILabel!
+    @IBOutlet var animal2Lbl : UILabel!
+    @IBOutlet var option1Lbl : UILabel!
+    @IBOutlet var option2Lbl: UILabel!
+    var animal1 : String = ""
+    var animal2 : String = ""
+    var option1 : String = ""
+    var option2 : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.title = "Result"
+        
+        animal1Lbl.text = animal1
+        animal2Lbl.text = animal2
+        option1Lbl.text = option1
+        option2Lbl.text = option2
 
         // Do any additional setup after loading the view.
     }
--- RandomChooser/SettingRandomViewController.swift
+++ RandomChooser/SettingRandomViewController.swift
@@ -13,12 +13,13 @@ class SettingRandomViewController: UIViewController {
     @IBOutlet var animal2 : UITextField!
     @IBOutlet var option1 : UITextField!
     @IBOutlet var option2 : UITextField!
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = "Setting Random option"
-
+      
         // Do any additional setup after loading the view.
     }
     
@@ -27,12 +28,18 @@ class SettingRandomViewController: UIViewController {
     }
 
     @IBAction func goToResultPage(){
+        
+        let options = [option1.text, option2.text].shuffled()
+
+        
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         
         let resultVC = storyboard.instantiateViewController(identifier: "ResultVC") as! ResultViewController
         
-       // resultVC.numWon = self.numWon
-        //resultVC.numLost = self.numLost
+        resultVC.animal1 = animal1.text!
+        resultVC.animal2 = animal2.text!
+        resultVC.option1 = options[0]!
+        resultVC.option2 = options[1]!
         
         
         self.navigationController?.pushViewController(resultVC, animated: true)
